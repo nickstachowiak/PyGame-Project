08fdc5c5431e27e63b333c47ebfcf4dc76fe5120
Nick Stachowiak <nick_stachowiak>
2022-02-25 18:35:16 -0600

    Make fleet move

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()   
 
     def _check_events(self):
@@ -80,6 +81,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        '''Check if the fleet is at an edge then update the positions of all aliens in the fleet'''
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         '''Create the fleet of aliens'''
         # Create an alien and find the number of aliens in a row
@@ -100,13 +106,26 @@ class AlienInvasion:
                 self._create_alien(alien_number, row_number)
 
     def _create_alien(self, alien_number, row_number):
-            # Create an alien and place it in the row
+            '''Create an alien and place it in the row'''
             alien = Alien(self)
             alien_width, alien_height = alien.rect.size
             alien.x = alien_width + 2 * alien_width * alien_number
             alien.rect.x = alien.x
-            alien.rect.y = alien_height + 2 * alien.rect.height + row_number
+            alien.rect.y = alien_height + 2 * alien.rect.height * row_number
             self.aliens.add(alien) 
+
+    def _check_fleet_edges(self):
+        '''Respond appropriately if any aliens have reached an edge'''
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        '''Drop the entire fleet and change the fleets direction'''
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         '''Update images on the screen, and flip to the new screen'''
